bb9d098cc7966af9b6c4d4ffb9f455334497296b
Alexandre Duarte <duartealexf@gmail.com>
2023-03-11 20:19:08 -0300

    refactor: add own code to get property

--- src/helpers/json.js
+++ src/helpers/json.js
@@ -1,9 +1,47 @@
-const get = require('lodash.get');
+const assert = require('assert');
 
 /**
  * @returns {JSONHelper}
  */
-module.exports = () => ({
-  /** @type {JSONHelper['getProperty']} */
-  getProperty: (object, path) => get(object, path),
-});
+module.exports = () => {
+  const validatePath = (path) => {
+    assert(typeof path === 'string', 'Property path must be a string');
+    assert(path !== '', 'Property path is empty');
+    assert(!/(\*).*\*/g.test(path), 'Property should have no more than one asterisk (*)');
+  };
+
+  const validateObject = (object) => {
+    assert(typeof object === 'object' && object !== null, 'Cannot get property of a non-object');
+  };
+
+  /**
+   * @param {unknown} object
+   * @param {string} path
+   * @returns {unknown}
+   */
+  const getProperty = (object, path) => {
+    const parts = path.split('.');
+    let current = object;
+
+    for (let i = 0; i < parts.length; i += 1) {
+      const part = parts[i];
+
+      if (typeof current !== 'object' || current === null) {
+        return undefined;
+      }
+
+      current = current[part];
+    }
+
+    return current;
+  };
+
+  return {
+    /** @type {JSONHelper['getProperty']} */
+    getProperty: (object, path) => {
+      validatePath(path);
+      validateObject(object);
+      return getProperty(object, path);
+    },
+  };
+};
--- tests/unit/json.test.js
+++ tests/unit/json.test.js
@@ -1,3 +1,4 @@
+const { AssertionError } = require('assert');
 const JSONHelper = require('../../src/helpers/json');
 
 const makeSut = () => JSONHelper();
@@ -5,6 +6,23 @@ const makeSut = () => JSONHelper();
 describe('json helper', () => {
   describe('getProperty', () => {
     describe('when the path is invalid', () => {
+      it('should throw an error when the path is not a string', () => {
+        const jsonHelper = makeSut();
+        expect(() => jsonHelper.getProperty({}, 1)).toThrow(new AssertionError({ message: 'Property path must be a string' }));
+      });
+
+      it('should throw an error when the path is empty', () => {
+        const jsonHelper = makeSut();
+        expect(() => jsonHelper.getProperty({}, '')).toThrow(new AssertionError({ message: 'Property path is empty' }));
+      });
+
+      it('should throw an error when the path has more than 1 star', () => {
+        const jsonHelper = makeSut();
+        expect(() => jsonHelper.getProperty({}, 'a.*.b.*')).toThrow(new AssertionError({ message: 'Property should have no more than one asterisk (*)' }));
+      });
+
+      it.todo('should throw an error when the star does not point to an array');
+
       it('should return undefined when the path is not found', () => {
         const jsonHelper = makeSut();
         expect(jsonHelper.getProperty({ a: { c: 'b' } }, 'a.c.b')).toBeUndefined();
@@ -13,6 +31,21 @@ describe('json helper', () => {
         expect(jsonHelper.getProperty([{ a: 1 }, { a: 2 }], '*.a')).toBeUndefined();
         expect(jsonHelper.getProperty([{ a: 1 }, { a: 2 }], 'a.b')).toBeUndefined();
         expect(jsonHelper.getProperty([{ a: 1 }, { a: 2 }], '0.b')).toBeUndefined();
+      });
+    });
+
+    describe('when the subject is invalid', () => {
+      it('should throw an error when the subject is not an object or array', () => {
+        const jsonHelper = makeSut();
+        const assertionError = new AssertionError({ message: 'Cannot get property of a non-object' });
+
+        expect(() => jsonHelper.getProperty('', 'a.b')).toThrow(assertionError);
+        expect(() => jsonHelper.getProperty(1, 'a.b')).toThrow(assertionError);
+        expect(() => jsonHelper.getProperty(false, 'a.b')).toThrow(assertionError);
+        expect(() => jsonHelper.getProperty(null, 'a.b')).toThrow(assertionError);
+        expect(() => jsonHelper.getProperty(undefined, 'a.b')).toThrow(assertionError);
+        expect(() => jsonHelper.getProperty([], 'a.b')).not.toThrow();
+        expect(() => jsonHelper.getProperty({}, 'a.b')).not.toThrow();
       });
     });
 
@@ -36,6 +69,56 @@ describe('json helper', () => {
         const jsonHelper = makeSut();
         expect(jsonHelper.getProperty({ a: { b: 'c' } }, 'a.b')).toStrictEqual('c');
         expect(jsonHelper.getProperty({ a: { b: 'c' } }, 'a')).toStrictEqual({ b: 'c' });
+      });
+
+      it('should map through items when star is the first path part', () => {
+        const jsonHelper = makeSut();
+        const subject = [
+          { a: 1, b: { c: 1 } },
+          { a: 2, b: { c: 2 } },
+          { a: 3, b: { d: 3 } },
+          { a: 4 },
+        ];
+
+        expect(jsonHelper.getProperty(subject, '*.b')).toStrictEqual([{ c: 1 }, { c: 2 }, { d: 3 }]);
+        expect(jsonHelper.getProperty(subject, '*.b.c')).toStrictEqual([1, 2]);
+      });
+
+      it('should map through items when star is not the first path part', () => {
+        const jsonHelper = makeSut();
+        const subject = {
+          a: [
+            { c: { d: 1 } },
+            { c: { d: 2 } },
+            { c: { d: 3 } },
+          ],
+        };
+
+        expect(jsonHelper.getProperty(subject, 'a.*.c')).toStrictEqual({ a: [{ d: 1 }, { d: 2 }, { d: 3 }] });
+      });
+
+      it('should get item in an index when index is the first path part', () => {
+        const jsonHelper = makeSut();
+        const subject = [
+          { a: [10, 20, { c: { d: 3 } }] },
+          { a: [11, 21, { c: { d: 4 } }] },
+          { a: [12, 22, { c: { d: 5 } }] },
+          { a: [12, 22, { }] },
+        ];
+
+        expect(jsonHelper.getProperty(subject, '2.a.*')).toStrictEqual([12, 22, { c: { d: 5 } }]);
+      });
+
+      it('should get item in an index when index is not the first path part', () => {
+        const jsonHelper = makeSut();
+        const subject = {
+          a: [
+            { c: { d: [{ e: 1 }, { e: 2 }, { f: 3 }] } },
+            { c: { d: [{ e: 4 }, { g: 5 }, { e: 6 }] } },
+          ],
+        };
+
+        expect(jsonHelper.getProperty(subject, 'a.1.c.d.*.e')).toStrictEqual([4, 6]);
       });
     });
   });
